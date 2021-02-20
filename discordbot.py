9657bf7fab485f0131968c92fc99fa2ecc084058
Antariska03 <54115861+Antariska03@users.noreply.github.com>
2021-02-20 21:27:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,27 +1,79 @@
-# インストールした discord.py を読み込む
-import discord
+import discord, asyncio, random
+from discord.ext import commands
+from itertools import accumulate
 
-# 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'os.environ['DISCORD_BOT_TOKEN']'
+import os
+import traceback
 
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
 
-# 起動時に動作する処理
+client = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
+
+
+# on_ready
 @client.event
 async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    
+    await client.change_presence(activity=discord.Game(name='/team'))
+            
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+# on command
+@client.command()
+async def team(ctx, count):
+    count = int(count)
+    
+    channel = ctx.author.voice.channel
+    l=[]
+    for member in channel.members:
+        l.append(member.name)
+    
+    n = len(l)
+    g = count
+    
+    embed_body = discord.Embed(title=f"{n} 人を {g} チームにシャッフル", colour=0x00a381)
+    
+    def shuffle_groups():
+        # Prepare group separators
+        size = n // g
+        rem = n % g
+        separators = list(accumulate([0] + [size+1] * rem + [size] * (g - rem)))
 
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+        # Make raw data
+        items = list(range(n))
+        random.shuffle(items)
+
+        # Iterate and print
+        for i, s in enumerate(zip(separators, separators[1:])):
+            group = items[slice(*s)]
+            print(f'Group {i+1}: {group} (size {len(group)})')
+            ul = []
+            for u in group:
+                ul.append(l[u])
+            name = f"チーム {i+1}: {len(group)} 人\n"
+            value = "\n".join(ul)
+            embed_body.add_field(name=name, value=value, inline=True)
+    
+    shuffle_groups()
+    
+    msg = await ctx.send(embed=embed_body)
+
+@client.command()
+async def count(ctx):
+    
+    channel = ctx.author.voice.channel
+    l=[]
+    for member in channel.members:
+        l.append(member.name)
+    
+    n = len(l)
+    
+    embed_body = discord.Embed(title=f"{channel.name} には {n} 人 参加しています", colour=0x00a381) 
+    
+    msg = await ctx.send(embed=embed_body)
+    
+# run the bot    
+client.run(token)
